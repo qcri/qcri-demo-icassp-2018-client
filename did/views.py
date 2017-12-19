e37d2032cf6cfe4c0df0e62027d79e6cef6799de
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-12-19 19:59:28 +0300

    update settings.py

--- did/views.py
+++ did/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 
+# def index(request):
+#     return HttpResponse("Hello, world! This is our first view.")
+
+
 def index(request):
-    return HttpResponse("Hello, world! This is our first view.")
+    # question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'did/index.html', {'data': 55})
