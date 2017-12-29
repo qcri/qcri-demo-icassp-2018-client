8fe06607932f079ba0f5b9643d7bbdf0c7eeee66
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2017-12-29 22:59:51 +0300

    Integrate new template

--- did/static/did/asr/examples/static-webapp-example/index.js
+++ did/static/did/asr/examples/static-webapp-example/index.js
@@ -42,15 +42,16 @@ var dictate = new Dictate({
         __status("");
     },
     onServerStatus: function (json) {
-        __serverStatus(json.num_workers_available + ':' + json.num_requests_processed);
+
+        __serverStatus(json.num_workers_available + ':disooqi:' + json.num_requests_processed);
     },
     onPartialResults: function (hypos, segmentid) {
         console.log("[PARTIAL] " + hypos[0]['transcript']);
-        // document.getElementById("transcripts").innerHTML = document.getElementById("transcripts").innerHTML + "<br/> " + "[PARTIAL] "  + hypos[0]['transcript'];
+        document.getElementById("asr-partial").innerHTML =  hypos[0]['transcript'];
     },
     onResults: function (hypos, segmentid) {
         console.log("[FINAL] " + hypos[0]['transcript']);
-        document.getElementById("transcripts").innerHTML = document.getElementById("transcripts").innerHTML + hypos[0]['transcript'] + " ";
+        document.getElementById("transcripts").innerHTML = document.getElementById("transcripts").innerHTML + hypos[0]['transcript'] + " \n ";
 
         var sortable = [];
         for (var vehicle in hypos[1]) {
@@ -60,13 +61,119 @@ var dictate = new Dictate({
         sortable.sort(function (a, b) {
             return b[1] - a[1];
         });
-        color_code = {"MSA":"success", "EGY":"danger", "GLF":"info", "NOR":"warning", "LEV":"active"}
-        document.getElementById("dialects").innerHTML = "<tr class=\""+ color_code[sortable[0][0]] +"\"><td>"+ sortable[0][0] +"</td><td>"+ sortable[0][1] +"</td><td>False</td></tr>" +
-            "<tr class=\""+ color_code[sortable[1][0]] +"\"><td>"+ sortable[1][0] +"</td><td>"+ sortable[1][1] +"</td><td>False</td></tr>" +
-            "<tr class=\""+ color_code[sortable[2][0]] +"\"><td>"+ sortable[2][0] +"</td><td>"+ sortable[2][1] +"</td><td>False</td></tr>" +
-            "<tr class=\""+ color_code[sortable[3][0]] +"\"><td>"+ sortable[3][0] +"</td><td>"+ sortable[3][1] +"</td><td>False</td></tr>" +
-            "<tr class=\""+ color_code[sortable[4][0]] +"\"><td>"+ sortable[4][0] +"</td><td>"+ sortable[4][1] +"</td><td>False</td></tr>";
-            //document.getElementById("dialects").innerHTML + "<br/> " + sortable;
+        color_code = {"MSA": "success", "EGY": "danger", "GLF": "info", "NOR": "warning", "LEV": "active"};
+        // document.getElementById("dialects").innerHTML = "<tr class=\""+ color_code[sortable[0][0]] +"\"><td>"+ sortable[0][0] +"</td><td>"+ sortable[0][1] +"</td><td>False</td></tr>" +
+        //     "<tr class=\""+ color_code[sortable[1][0]] +"\"><td>"+ sortable[1][0] +"</td><td>"+ sortable[1][1] +"</td><td>False</td></tr>" +
+        //     "<tr class=\""+ color_code[sortable[2][0]] +"\"><td>"+ sortable[2][0] +"</td><td>"+ sortable[2][1] +"</td><td>False</td></tr>" +
+        //     "<tr class=\""+ color_code[sortable[3][0]] +"\"><td>"+ sortable[3][0] +"</td><td>"+ sortable[3][1] +"</td><td>False</td></tr>" +
+        //     "<tr class=\""+ color_code[sortable[4][0]] +"\"><td>"+ sortable[4][0] +"</td><td>"+ sortable[4][1] +"</td><td>False</td></tr>";
+        //document.getElementById("dialects").innerHTML + "<br/> " + sortable;
+
+
+        // function setDefColor(element, index, array) {
+        //
+        //
+        // }
+
+        var updatedOptions = {'areas': {}};
+
+        // for (var property in $("#map").data("mapael").areas) {
+        //     console.log(property);
+        // }
+
+        var countriesOfDialect = {
+            "EGY": ["eg"],
+            "MSA": ["sd"],
+            "GLF": ["sa", "qa", "om", "kw", "ae"],
+            "NOR": ["eh", "ma", "dz", "tn", "ly"],
+            "LAV": ["sy", "jo", "ps", "lb"]
+        };
+        $("#"+sortable[0][0].toLowerCase()).css("width", parseFloat(sortable[0][1])*100+"%");
+        $("#"+sortable[1][0].toLowerCase()).css("width", parseFloat(sortable[1][1])*100+"%");
+        $("#"+sortable[2][0].toLowerCase()).css("width", parseFloat(sortable[2][1])*100+"%");
+        $("#"+sortable[3][0].toLowerCase()).css("width", parseFloat(sortable[3][1])*100+"%");
+        $("#"+sortable[4][0].toLowerCase()).css("width", parseFloat(sortable[4][1])*100+"%");//percent-egy
+
+        $("#percent-"+sortable[0][0].toLowerCase()).text(Math.ceil(parseFloat(sortable[0][1])*100));
+        $("#percent-"+sortable[1][0].toLowerCase()).text(Math.ceil(parseFloat(sortable[1][1])*100));
+        $("#percent-"+sortable[2][0].toLowerCase()).text(Math.ceil(parseFloat(sortable[2][1])*100));
+        $("#percent-"+sortable[3][0].toLowerCase()).text(Math.ceil(parseFloat(sortable[3][1])*100));
+        $("#percent-"+sortable[4][0].toLowerCase()).text(Math.ceil(parseFloat(sortable[4][1])*100));
+
+        var $map = $('#map');
+
+        Object.keys($map.data("mapael").areas).forEach(function (key, index) {
+            // console.log(sortable[0][0]);
+            if (countriesOfDialect[sortable[0][0]].indexOf(key) > -1) {
+
+                updatedOptions.areas[key] = {
+                    attrs: {
+                        fill: "#f38a03"
+                    }
+                };
+            } else if (countriesOfDialect[sortable[1][0]].indexOf(key) > -1) {
+                // $("#egy").css("width", parseFloat(sortable[1][1])*100+"%");
+                updatedOptions.areas[key] = {
+                    attrs: {
+                        fill: "#915201"
+                    }
+                };
+            } else if (countriesOfDialect[sortable[2][0]].indexOf(key) > -1) {
+                // $("#egy").css("width", parseFloat(sortable[2][1])*100+"%");
+                updatedOptions.areas[key] = {
+                    attrs: {
+                        fill: "#482900"
+                    }
+                };
+            } else if (countriesOfDialect[sortable[3][0]].indexOf(key) > -1) {
+                // $("#egy").css("width", parseFloat(sortable[3][1])*100+"%");
+                updatedOptions.areas[key] = {
+                    attrs: {
+                        fill: "#343434"
+                    }
+                };
+            } else {
+                updatedOptions.areas[key] = {
+                    attrs: {
+                        fill: "#343434"
+                    }
+                };
+                // block of code to be executed if the condition1 is false and condition2 is false
+            }
+
+
+        });
+
+
+        // updatedOptions.areas["sa"] = {
+        //     attrs: {
+        //         fill: "#ff0000"
+        //     }
+        // };
+        // updatedOptions.areas["qa"] = {
+        //     attrs: {
+        //         fill: "#00ff00"
+        //     }
+        // };
+        // updatedOptions.areas["om"] = {
+        //     attrs: {
+        //         fill: "#00ff00"
+        //     }
+        // };
+        // updatedOptions.areas["ae"] = {
+        //     attrs: {
+        //         fill: "#00ff00"
+        //     }
+        // };
+        // updatedOptions.areas["kw"] = {
+        //     attrs: {
+        //         fill: "#00ff00"
+        //     }
+        // };
+        $("#map").trigger('update', [{
+            mapOptions: updatedOptions,
+            animDuration: 1000
+        }]);
 
     },
     onError: function (code, data) {
